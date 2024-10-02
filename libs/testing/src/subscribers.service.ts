bf2da552053c370f8e6dad9ad4341857eb292fb8
vymoo <107595054+vymoo@users.noreply.github.com>
2024-10-02 17:21:50 +0800

    Create subscribers.service.ts

---
+++ libs/testing/src/subscribers.service.ts
@@ -0,0 +1,33 @@
+import { faker } from '@faker-js/faker';
+import { SubscriberRepository } from '@novu/dal';
+import { ChatProviderIdEnum, PushProviderIdEnum } from '@novu/shared';
+
+export class SubscribersService {
+  private subscriberRepository = new SubscriberRepository();
+
+  constructor(private _organizationId: string, private _environmentId: string) {}
+
+  async createSubscriber() {
+    return await this.subscriberRepository.create({
+      lastName: faker.name.lastName(),
+      firstName: faker.name.firstName(),
+      email: faker.internet.email(),
+      phone: faker.phone.phoneNumber(),
+      _environmentId: this._environmentId,
+      _organizationId: this._organizationId,
+      subscriberId: SubscriberRepository.createObjectId(),
+      channels: [
+        {
+          _integrationId: 'integrationId_slack',
+          providerId: ChatProviderIdEnum.Slack,
+          credentials: { webhookUrl: 'webhookUrl' },
+        },
+        {
+          _integrationId: 'integrationId_fcm',
+          providerId: PushProviderIdEnum.FCM,
+          credentials: { deviceTokens: ['identifier'] },
+        },
+      ],
+    });
+  }
+}
