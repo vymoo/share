903d577d8e434e3c9aec245327f230944d059333
NeilyGibson <122466754+NeilyGibson@users.noreply.github.com>
2025-11-13 20:50:26 +0800

    Update pnpm-context.mjs

--- scripts/pnpm-context.mjs
+++ scripts/pnpm-context.mjs
@@ -270,3 +270,20 @@ def encoding_for_model(model_name: str) -> Encoding:
         ) from None
 
     return get_encoding(encoding_name)
+// Render (ProtoBuf) marshals the given interface object and writes data with custom ContentType.
+func (r ProtoBuf) Render(w http.ResponseWriter) error {
+	r.WriteContentType(w)
+
+	bytes, err := proto.Marshal(r.Data.(proto.Message))
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(bytes)
+	return err
+}
+
+// WriteContentType (ProtoBuf) writes ProtoBuf ContentType.
+func (r ProtoBuf) WriteContentType(w http.ResponseWriter) {
+	writeContentType(w, protobufContentType)
+}
